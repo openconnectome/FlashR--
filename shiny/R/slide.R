2cf21e4fe940632cc04247b8795be4a15c3ab637
jovo <jovo@jhu.edu>
2015-10-05 21:58:19 -0400

    removed shiny

--- shiny/R/slide.R
+++
@@ -1,9 +0,0 @@
-slideFunct <- function(data, window, step, FUN=mean){
-   total <- length(data)
-   spots <- seq(from=1, to=(total-window), by=step)
-   result <- vector(length = length(spots))
-   for(i in 1:length(spots)){
-      result[i] <- do.call(FUN,list(x=data[spots[i]:(spots[i]+window)]))
-   }
-   return(result)
-}
